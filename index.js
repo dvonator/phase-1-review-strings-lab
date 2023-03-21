4aec679b239a78fd66e67d82401398421113590b
David <davidvo1992@gmail.com>
2023-03-21 11:22:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "David Vo";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`; // remember to add parenthesis after the .touppercase etc etc
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
